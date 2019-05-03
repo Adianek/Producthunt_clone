96ae8a134c55e3df34a3f08589bee604acec6c46
Adianek <adamswidzikowski@MacBook-Pro-Adam.local>
2019-05-03 12:28:18 +0200

    Detail view added

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
@@ -11,7 +11,8 @@ def home(request):
 @login_required
 def create(request):
     if request.method == 'POST':
-        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.POST['icon'] and request.POST['image']:
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.POST['icon'] and \
+                request.POST['image']:
             product = Product()
             product.title = request.POST['title']
             product.body = request.POST['body']
@@ -24,9 +25,14 @@ def create(request):
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
             product.save()
-            return redirect('home')
+            return redirect('/products/' + str(product.id))
         else:
-            return render(request, "products/create.html", {'error':'All fields are required.'})
+            return render(request, "products/create.html", {'error': 'All fields are required.'})
 
     else:
         return render(request, "products/create.html")
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
