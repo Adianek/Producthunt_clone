7ea184bd12b8ed99e3905a129e404b5e959fd470
Adianek <adamswidzikowski@MacBook-Pro-Adam.local>
2019-05-07 22:31:02 +0200

    Web app working properly, but need some style

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:product_id>', views.detail, name='detail'),
+    path('<int:product_id>/upvote>', views.upvote, name='upvote'),
 ]
--- products/views.py
+++ products/views.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 
 
 def home(request):
-    return render(request, "products/home.html")
+    products = Product.objects
+    return render(request, "products/home.html", {'products':products})
 
 
 @login_required
@@ -36,3 +37,11 @@ def create(request):
 def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html', {'product': product})
+
+@login_required
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))
